359b458e636038fac41a7a0202a2f1f6bb78accc
dk <kukkudanish@gmail.com>
2026-02-16 15:46:55 +0530

    setup backend as live

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
-// const adminRoutes = require("./routes/adminRoutes");
-// const settingsRoutes = require("./routes/settingsRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/settings", settingsRoutes);
 
 module.exports = app;
