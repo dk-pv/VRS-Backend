e22d54e8664c9d74764cfb7dfc6356048816d77f
dk <kukkudanish@gmail.com>
2026-04-21 14:12:06 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,54 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const authRoutes = require("./routes/authRoutes");
+// const heroRoutes = require("./routes/heroRoutes");
+// const securedPropertyRoutes = require("./routes/securedPropertyRoutes");
+// const webinarRoutes = require("./routes/webinarRoutes");
+// const discoverVideoRoutes = require("./routes/discoverVideoRoutes");
+// const teamRoutes = require("./routes/teamRoutes");
+// const textTestimonialRoutes = require("./routes/textTestimonialRoutes");
+// const videoTestimonialRoutes = require("./routes/videoTestimonialRoutes");
+// const blogRoutes = require("./routes/blogRoutes");
+// const dashboardRoutes = require("./routes/dashboardRoutes");
+// const app = express();
+
+// const allowedOrigins = [
+//   process.env.USER_FRONTEND_URL,
+//   process.env.ADMIN_FRONTEND_URL,
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   }),
+// );
+
+// app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/hero", heroRoutes);
+// app.use("/api/secured-properties", securedPropertyRoutes);
+// app.use("/api/webinars", webinarRoutes);
+// app.use("/api/discover-video", discoverVideoRoutes);
+// app.use("/api/team", teamRoutes);
+// app.use("/api/text-testimonials", textTestimonialRoutes);
+// app.use("/api/video-testimonials", videoTestimonialRoutes);
+// app.use("/api/blog", blogRoutes);
+// app.use("/api/dashboard", dashboardRoutes);
+
+// module.exports = app;
+
+
+
+
 const express = require("express");
 const cors = require("cors");
 
@@ -11,24 +62,15 @@ const textTestimonialRoutes = require("./routes/textTestimonialRoutes");
 const videoTestimonialRoutes = require("./routes/videoTestimonialRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+
 const app = express();
 
-const allowedOrigins = [
-  process.env.USER_FRONTEND_URL,
-  process.env.ADMIN_FRONTEND_URL,
-];
-
+// Allow all origins
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());
